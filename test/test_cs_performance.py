ab405e5d6474c2221313d47fc862e6ff72d8a9ad
IvanOleksiyuk <ivan.oleksiyuk@gmail.com>
2022-12-17 17:20:58 +0100

    improved test namings

--- test/test_cs_performance.py
+++ test/test_cs_performance.py
@@ -14,23 +14,23 @@ class TestCSPerformance(unittest.TestCase):
             sys.path[0] + "/test_materials/count_windows.npy"
         )
 
-    def test_m5sigma(self):
-        """Test evluation of >5sigma method"""
+    def test_maxdev5(self):
+        """Test evluation of maxdev5 method"""
         res = cs_performance_evaluation(
             counts_windows=self.counts_windows,
-            save_path="test_results/test_m5sigma/",
+            save_path="test_results/test_maxdev5/",
             filterr="med",
             plotting=False,
-            labeling=">5sigma",
+            labeling="maxdev5",
             verbous=False,
         )
         self.assertAlmostEqual(3.508741407313602, res["chisq_ndof"])
 
-    def test_other(self):
-        """Test evluation of  method"""
+    def test_2meansder(self):
+        """Test evluation of 2meansder method"""
         res = cs_performance_evaluation(
             counts_windows=self.counts_windows,
-            save_path="test_results/test_other/",
+            save_path="test_results/test_2meansder/",
             filterr="med",
             plotting=False,
             labeling="2meansder",
@@ -39,18 +39,19 @@ class TestCSPerformance(unittest.TestCase):
         self.assertAlmostEqual(3.5990916567517726, res["chisq_ndof"])
         pass
 
-    def test_m5sigma_with_plotting(self):
+    def test_maxdev5_with_plotting(self):
         """Test evluation of >5sigma method"""
         res = cs_performance_evaluation(
             counts_windows=self.counts_windows,
-            save_path="test_results/test_m5sigma_with_plotting/",
+            save_path="test_results/test_maxdev5_with_plotting/",
             filterr="med",
             plotting=True,
-            labeling=">5sigma",
+            labeling="maxdev5",
             verbous=False,
         )
 
 
-TT = TestCSPerformance()
-TT.setUp()
-TT.test_m5sigma_with_plotting()
+if __name__ == "__main__":
+    TT = TestCSPerformance()
+    TT.setUp()
+    TT.test_maxdev5_with_plotting()
