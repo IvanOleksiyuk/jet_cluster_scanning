ba4c992e91ef8f7fb682d3193ab35d24327b17c3
IvanOleksiyuk <ivan.oleksiyuk@gmail.com>
2023-07-21 12:00:24 +0200

    fixed some bugs in dijet_full_fit.ipynb

--- utils/test_statistics.py
+++ utils/test_statistics.py
@@ -1,4 +1,7 @@
 import numpy as np
 
 def MLSnormal_positiv(bkg, sig):
-    return np.max((bkg-sig)/np.sqrt(bkg))
+    return np.max((sig-bkg)/np.sqrt(bkg))
+
+def chi_square(bkg, sig):
+    return np.sum((sig-bkg)**2/bkg)
